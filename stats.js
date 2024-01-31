d2fae7324ddd1b3aa3e4f58fe125be203c533dd0
stian.larsen <stian.larsen@sparebank1.no>
2024-01-31 15:54:13 +0100

    new

--- stats.js
+++ stats.js
@@ -30,25 +30,51 @@ export function updateStatsForThread(
 
     if (date) {
       const insightsPath = path.join(insightsDir, `${channelId}.json`);
-      fs.readFile(insightsPath, (err, data) => {
-        if (err) throw err;
-        const stats = JSON.parse(data);
-
-        if (!stats[date]) {
-          stats[date] = {};
-        }
-
-        if (!stats[date][userId]) {
-          stats[date][userId] = 0;
-        }
-
-        stats[date][userId] += repetitions;
-
-        fs.writeFile(insightsPath, JSON.stringify(stats), (err) => {
+      // Check if the file exists
+      if (fs.existsSync(insightsPath)) {
+        // File exists, read and update it
+        fs.readFile(insightsPath, (err, data) => {
           if (err) throw err;
-          console.log("Stats updated for user", userId, "on", date);
+
+          const stats = JSON.parse(data);
+
+          if (!stats[date]) {
+            stats[date] = {};
+          }
+
+          if (!stats[date][userId]) {
+            stats[date][userId] = 0;
+          }
+
+          stats[date][userId] += repetitions;
+
+          fs.writeFile(insightsPath, JSON.stringify(stats, null, 2), (err) => {
+            if (err) throw err;
+            console.log("Stats updated for user", userId, "on", date);
+          });
         });
-      });
+      } else {
+        // File does not exist, create it with the initial data
+        const initialStats = {
+          [date]: {
+            [userId]: repetitions,
+          },
+        };
+
+        fs.writeFile(
+          insightsPath,
+          JSON.stringify(initialStats, null, 2),
+          (err) => {
+            if (err) throw err;
+            console.log(
+              "Stats file created and updated for user",
+              userId,
+              "on",
+              date
+            );
+          }
+        );
+      }
     }
   });
 }
